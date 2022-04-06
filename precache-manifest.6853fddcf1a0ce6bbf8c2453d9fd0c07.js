6e77567914cbfadb5e8c87f522b26cb054b2b684
Saul Rotter <saul.rotter@gmail.com>
2022-04-05 23:40:53 -0700

    Updates

---
+++ precache-manifest.6853fddcf1a0ce6bbf8c2453d9fd0c07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78db4e51cf016223f8cf",
+    "url": "/static/css/main.850ec3b3.chunk.css"
+  },
+  {
+    "revision": "78db4e51cf016223f8cf",
+    "url": "/static/js/main.e2dcbcb7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58518ca7a856dbc6c8b4",
+    "url": "/static/js/2.185a0e82.chunk.js"
+  },
+  {
+    "revision": "1f95defa71c0420aae71c359a2643b77",
+    "url": "/index.html"
+  }
+];
