d267dbb813f2f1cb8084436ef3747cb31743abc7
Saul Rotter <saul.rotter@gmail.com>
2022-04-19 17:39:29 -0400

    Update build to use my homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,11 @@ function PlayerNavigation({ token }) {
       style={{ height: "min-content", width: "max-content" }}
     >
       <Navbar.Brand href="/react_app" style={{ width: "20%", paddingRight: 5 }}>
-        <img src={"/trumedialogo.png"} alt={"Logo"} className="img-thumbnail" />
+        <img
+          src={"/react_app/trumedialogo.png"}
+          alt={"Logo"}
+          className="img-thumbnail"
+        />
       </Navbar.Brand>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav style={{ width: "25%", paddingRight: 10 }}>{content}</Nav>
