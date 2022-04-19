417f3c800745bbc7715fa09027bfb446ed46b1a2
Saul Rotter <saul.rotter@gmail.com>
2022-04-19 14:54:22 -0400

    Logo Fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,32 +5,34 @@ import { useGetPlayersQuery } from '../features/players/playersSlice';
 
 
 function PlayerNavigation({ token }) {
-  console.log(token);
-  const {
-    data: players = [],
-    isSuccess,
-  } = useGetPlayersQuery(token);
+  const { data: players = [], isSuccess } = useGetPlayersQuery(token);
   let content = null;
-  
+
   if (isSuccess) {
-    content = players.map(player => <Nav.Item style={{padding: 5}} key={player.playerId}><PlayerLink player={player}/></Nav.Item>);
+    content = players.map((player) => (
+      <Nav.Item style={{ padding: 5 }} key={player.playerId}>
+        <PlayerLink player={player} />
+      </Nav.Item>
+    ));
   }
   return (
-    <Navbar collapseOnSelect bg="dark" variant="dark" style={{height: "min-content", width:"max-content"}}>
-        <Navbar.Brand href='/' style={{width: "20%", paddingRight: 5}}>
-          <img
-            src={'../../trumedialogo.png'}
-            alt={'Logo'}
-            className="img-thumbnail"
-          />
-        </Navbar.Brand>
-        <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav style={{width: "25%", paddingRight: 10}}>
-          {content}
-        </Nav>
-        </Navbar.Collapse>
+    <Navbar
+      collapseOnSelect
+      bg="dark"
+      variant="dark"
+      style={{ height: "min-content", width: "max-content" }}
+    >
+      <Navbar.Brand href="/" style={{ width: "20%", paddingRight: 5 }}>
+        <img
+          src={"../../public/trumedialogo.png"}
+          alt={"Logo"}
+          className="img-thumbnail"
+        />
+      </Navbar.Brand>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav style={{ width: "25%", paddingRight: 10 }}>{content}</Nav>
+      </Navbar.Collapse>
     </Navbar>
-    
   );
 }
 
